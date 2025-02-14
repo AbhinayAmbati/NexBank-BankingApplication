4407bf16fe4b50d143d671135e53d0e14147dd55
AbhinayAmbati <abhinayambati4@gmail.com>
2025-02-15 01:42:34 +0530

    Implement sidebar and mobilenav

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 
 const Home = () => {
+
+  const isLoggedIn = { firstName  :"Abhinay"}
+
   return (
-    <div>
-      HOME
-    </div>
+    <section className='home'>
+      <div className="home-content">
+        <header className='home-header'>
+          <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={isLoggedIn?.firstName || 'Guest'}
+          subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+          accounts = {[]}
+          totalBanks = {1}
+          totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
