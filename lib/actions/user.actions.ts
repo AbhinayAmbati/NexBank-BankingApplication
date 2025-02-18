f9e258e43d4d4bcdb8beaeeb460cb1fe18e5d91c
AbhinayAmbati <abhinayambati4@gmail.com>
2025-02-18 12:44:56 +0530

    .

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -85,10 +85,8 @@ export const signUp = async ({ password, ...userData }: SignUpParams) => {
     } catch (error) {
       console.error('Error', error);
     }
-  }
+}
   
-
-
 export async function getLoggedInUser() {
     try {
       const { account } = await createSessionClient();
