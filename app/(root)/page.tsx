7aea409d5ca700331dce9fd9802a4632c63179b8
AbhinayAmbati <abhinayambati4@gmail.com>
2025-02-22 23:46:14 +0530

    Implement displaying dynamic data of bank accounts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-const Home = async () => {
+const Home = async ({ searchParams : {id, page}} : SearchParamProps) => {
 
   const isLoggedIn = await getLoggedInUser();
+
+  const accounts = await getAccounts({ userId: isLoggedIn.$id });
+
+  if(!accounts) return;
+
+  const accountsData = accounts?.data;
+
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId });
   
   return (
     <section className='home'>
@@ -14,21 +26,21 @@ const Home = async () => {
           <HeaderBox
           type="greeting"
           title="Welcome"
-          user={isLoggedIn?.name || 'Guest'}
+          user={isLoggedIn?.firstName || 'Guest'}
           subtext="Access and manage your account and transactions efficiently."
           />
           <TotalBalanceBox
-          accounts = {[]}
-          totalBanks = {1}
-          totalCurrentBalance={1250.35}
+          accounts = {accountsData}
+          totalBanks = {accounts?.totalBanks}
+          totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
         RECENT TRANSACTIONS
       </div>
       <RightSidebar
       user={isLoggedIn}
-      transactions={[]}
-      banks={[{currentBalance: 123.50},{currentBalance: 250.50}]}
+      transactions={accountsData?.transactions}
+      banks={accountsData?.slice(0,2)}
       />
     </section>
   )
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 'use server';
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
 import { encryptId, extractCustomerIdFromUrl, parseStringify } from "../utils";
@@ -17,18 +17,38 @@ const {
     APPWRITE_BANK_COLLECTION_ID : USER_BANK_ID,
 } = process.env; 
 
+
+export const getUserInfo = async ({ userId }: getUserInfoProps) => {
+  try {
+    const { Database } = await createAdminClient();
+
+    const user = await Database.listDocuments(
+      DATABASE_ID!,
+      USER_COLLECTION_ID!,
+      [Query.equal('userId', [userId])]
+    )
+
+    return parseStringify(user.documents[0]);
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export const signIn = async ({email, password} : signInProps) => {
     try{
         const { account } = await createAdminClient();
-        const response = await account.createEmailPasswordSession(email, password);
+        const session = await account.createEmailPasswordSession(email, password);
         const cookieStore = await cookies();
-        cookieStore.set("appwrite-session", response.secret, {
+        cookieStore.set("appwrite-session", session.secret, {
             path: "/",
             httpOnly: true,
             sameSite: "strict",
             secure: true,
         });
-        return parseStringify(response);
+
+        const user = await getUserInfo({ userId : session.userId });
+
+        return parseStringify(user);
     }catch(err){
         console.error(err);
     }
@@ -90,7 +110,10 @@ export const signUp = async ({ password, ...userData }: SignUpParams) => {
 export async function getLoggedInUser() {
     try {
       const { account } = await createSessionClient();
-      const user =  await account.get();
+      const result =  await account.get();
+
+      const user = await getUserInfo({ userId: result.$id });
+
       return parseStringify(user);
     } catch (error) {
       return error;
@@ -219,3 +242,23 @@ export const exchangePublicToken = async ({
       console.error("An error occurred while creating exchanging token:", error);
     }
   }
+
+export const getBanks = async ({ userId } : getBanksProps) => {
+    try{
+        const { Database } = await createAdminClient();
+        const banks = await Database.listDocuments(DATABASE_ID!, USER_BANK_ID!, [Query.equal('userId', userId)]);
+        return parseStringify(banks.documents);
+    }catch(err){
+        console.error(err);
+    }
+}
+
+export const getBank = async ({ documentId } : getBankProps) => {
+  try{
+      const { Database } = await createAdminClient();
+      const bank = await Database.listDocuments(DATABASE_ID!, USER_BANK_ID!, [Query.equal('$id', [documentId])]);
+      return parseStringify(bank.documents[0]);
+  }catch(err){
+      console.error(err);
+  }
+}
